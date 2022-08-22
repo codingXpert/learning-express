8583fca51cc0d471421d8a472924c50db6309639
Vivek <vivekraj78965.vr@gmail.com>
2022-08-22 07:51:01 +0530

    adding static pages

---
+++ src/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 8000;
+
+
+const static_path = path.join(__dirname , "../public");
+
+// builtin middleware
+app.use(express.static(static_path));
+
+
+
+//routes
+app.get('' , (req , res)=>{
+    res.send('welcome to my home page');
+});
+
+app.get('/about' , (req , res)=>{
+    res.send('welcome to my about page');
+});
+
+app.get('/contact' , (req , res)=>{
+    res.send('welcome to my contact page');
+});
+
+app.get('/temp' , (req , res)=>{
+    res.send('welcome to my page');
+});
+
+app.get('/json' , (req , res)=>{
+    res.send(
+        {
+            id : 1,
+            name:"vinod"
+        },
+    );
+});
+
+app.get('/arrJson' , (req , res)=>{
+    res.send([
+        {
+            id : 1,
+            name:"vinod"
+        },
+        {
+            id : 2,
+            name:"rohan"
+        },
+        {
+            id : 3,
+            name:"pratap"
+        },
+        {
+            id:null,
+            name:null
+        }
+    ]);
+});
+
+app.get('/resjson' , (req , res)=>{
+    res.json([   //sending data using res.json
+        {
+            id : 1,
+            name:"vinod"
+        },
+        {
+            id : 2,
+            name:"rohan"
+        },
+        {
+            id : 3,
+            name:"pratap"
+        },
+        {
+            id:null,
+            name:null
+        }
+    ]);
+});
+
+
+
+
+
+
+app.listen(port , (err)=>{
+    if(err){
+        console.log(`Error to connecying to server bue to ${err}`);
+    }
+    console.log(`Server running on the port :: ${port}`);
+})
